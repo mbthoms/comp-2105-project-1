ea2345b0dd072d76d3869b0353c1ba94339d4b5f
Matthew Thoms <mbthoms@outlook.com>
2016-03-07 18:14:37 -0500

    first-commit

---
+++ js/main-js.js
@@ -0,0 +1,10 @@
+// JS Document
+$( document ).ready(function() {
+  $("input[type='checkbox']").change(function(){
+      if($(this).is(":checked")){
+          $('body').addClass("greenBackground");
+      }else{
+          $('body').removeClass("greenBackground");
+      }
+  });
+});
